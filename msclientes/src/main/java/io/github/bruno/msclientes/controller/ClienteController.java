5729717351a241102e5759385662c36d643a7799
Bruno Santos <bruno_st17@hotmail.com>
2023-05-06 10:47:15 -0300

    Configurando o cloud gateway

--- msclientes/src/main/java/io/github/bruno/msclientes/controller/ClienteController.java
+++ msclientes/src/main/java/io/github/bruno/msclientes/controller/ClienteController.java
@@ -3,6 +3,7 @@ package io.github.bruno.msclientes.controller;
 import io.github.bruno.msclientes.dto.ClienteDto;
 import io.github.bruno.msclientes.entity.ClienteEntity;
 import io.github.bruno.msclientes.service.ClienteService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
+@Slf4j
 public class ClienteController {
 
     @Autowired
@@ -20,13 +22,13 @@ public class ClienteController {
 
     @GetMapping
     public String status() {
+        log.info("obtendo o status do microservice de cliente");
         return "ok";
     }
 
     @PostMapping
     public ResponseEntity saveUser(@RequestBody ClienteDto client) {
         ClienteEntity clienteEntity = ClienteDto.toModel(client);
-        System.out.println(clienteEntity);
         service.save(clienteEntity);
         URI headerLocation = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -37,11 +39,7 @@ public class ClienteController {
     @GetMapping("/{cpf}")
     public ResponseEntity getDataClient(@PathVariable("cpf") String cpf) {
         Optional<ClienteEntity> cliente = service.getByCpf(cpf);
-        System.out.println("aaaa");
-        System.out.println(cliente);
-        if (cliente.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
+        if (cliente.isEmpty()) return ResponseEntity.notFound().build();
         return ResponseEntity.ok().body(cliente);
     }
 }
