872030a02b14f56ccc716581ad295e66e60909a4
Bruno Santos <bruno_st17@hotmail.com>
2023-05-10 14:27:13 -0300

    Finalizando de configurar o keycloack e configurando o security no eureka-server

---
+++ eurekaserver/src/main/java/io/github/bruno/eurekaserver/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.github.bruno.eurekaserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
